4bc9e924e457b5bf7cda5fb0141d08ebb15a4d60
qkrthdus3203 <qkrthdus3203@naver.com>
2021-03-19 18:03:59 +0900

    21.03.19(금)

---
+++ yedamEmpl/src/myapp/FxmlApp.java
@@ -0,0 +1,25 @@
+package myapp;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class FxmlApp extends Application {
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		Parent root = FXMLLoader.load(getClass().getResource("AnchorPane.fxml")); // Label, Button
+		
+		// 컨테이너를 Scene의 매개값으로.
+		Scene scene = new Scene(root);
+		
+		//Stage의 매개값으로 Scene 달아줌.
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+	
+	public static void main(String[] args) {
+		Application.launch(args);
+	}
+}
